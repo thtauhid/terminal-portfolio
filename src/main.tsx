f7624c38d9655f5c317ed9d3a4ecea4fa617d6f9
Divyanshi <divyanshi.narang16@gmail.com>
2023-10-02 20:16:09 +0530

    prettier

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-          <React.StrictMode>
+  <React.StrictMode>
     <App />
     <Analytics />
   </React.StrictMode>,
