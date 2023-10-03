c2f53f90d88ff91af4a248d32d695be650b7546f
Y1a2s3h4 <yashpurohit763@gmail.com>
2023-10-03 16:05:22 +0530

    merge: merged and solved conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
   ]);
   const [userInput, setUserInput] = useState("");
   const executeCommand = (command: string) => {
-    if (!(command.trim().startsWith("setname"))) {
+    if (command.trim().startsWith("setname")) {
       command = command;
       //Handled by previous commit
     } else {
