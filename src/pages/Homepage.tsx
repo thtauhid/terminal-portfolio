53c21a47a8de7ca07a07e3b9019778eb71041fd2
Y1a2s3h4 <yashpurohit763@gmail.com>
2023-10-05 14:30:23 +0530

    merge: merge visitPortfolio branch in main

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -68,11 +68,15 @@ function Homepage() {
           (option) => option.label === command
         )!.data;
 
-        output += data?.map((item) => {
-          return `<br /><br />
-              <strong>${item.label}</strong> <br /> 
-              ${item.value}`;
-        });
+        if(command === "projects"){
+          output += data?.map((item) => {
+            return `<br /><br /> <strong><a class="underline" href="${item.url}" target="_blank" rel="noopener norefferer">${item.label}</a></strong> <br /> ${item.value}`;
+          });
+        } else {
+          output += data?.map((item) => {
+            return `<br /><br /> <strong>${item.label}</strong> <br /> ${item.value}`;
+          });
+        }
       }
 
       setHistory((history) => [
