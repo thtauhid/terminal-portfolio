c5f7cacc6833f85a7ba665ed3a7600f6fbadd7ce
Amy Dang <kirontoo@gmail.com>
2023-10-06 14:06:49 -0700

    center x symbole #23

--- src/components/WindowTitleBar.tsx
+++ src/components/WindowTitleBar.tsx
@@ -5,7 +5,7 @@ const WindowTitleBar = (props: { customUserName: string }) => {
     <div className="flex flex-row bg-stone-600 w-screen px-2 text-stone-700 gap-4 py-1 items-center">
       <span className="flex flex-row gap-2">
         <div className="outline outline-1 outline-stone-700 rounded-full flex justify-center items-center bg-orange-700 h-5 w-5">
-          &#120;
+          &times;
         </div>
         <div className="outline outline-1 outline-stone-700 rounded-full flex justify-center items-center bg-neutral-500 h-5 w-5">
           &#45;
